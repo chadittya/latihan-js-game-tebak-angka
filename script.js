49924d1aeebdc1ca2970a06c6fc09fa7c326fb1c
Galih Arizza <chadittya@gmail.com>
2022-10-06 12:13:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var chances = 3,
+  pGuess = parseInt(prompt("Tebak Angka antar 1-10: ")),
+  pChances = 1,
+  output = "";
+
+// get random number AI
+var numAnswer = Math.floor(Math.random() * 10) + 1;
+// console.log(numAnswer);
+
+while (pChances < chances) {
+  if (pGuess == numAnswer) {
+    pChances += 3;
+    output = alert("Angka yang kamu tebak BENAR!!!\nAngkanya adalah: " + numAnswer);
+  } else if (pGuess < numAnswer) {
+    output = confirm("Angka kurang BESAR\nKesempatanmu tinggal: " + (chances - pChances) + "\nMau tebak lagi?");
+    pGuess = parseInt(prompt("Tebak Angka antar 1-10: "));
+  } else if (pGuess > numAnswer) {
+    output = confirm("Angka kurang KECIL\nKesempatanmu tinggal: " + (chances - pChances) + "\nMau tebak lagi?");
+    pGuess = parseInt(prompt("Tebak Angka antar 1-10: "));
+  } else {
+    pChances += 3;
+    output = alert("Inputan bukan angka");
+  }
+  pChances++;
+}
+if (pChances >= chances && pGuess != numAnswer) {
+  output = alert("Kesempatan habis, angka yang benar adalah: " + numAnswer + "\nTerimakasih sudah bermain!!!");
+} else {
+  alert("Terimakasih sudah bermain!!!");
+}
